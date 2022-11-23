a365433b9c46ed979caf00655ac93969e4debc78
Maciej Lisowski <macieejl00@gmail.com>
2022-11-23 22:49:31 +0100

    closer to the solution

--- Optymalizacja/optymalizacja_lab_1_pliki_zrodlowe/opt_alg.cpp
+++ Optymalizacja/optymalizacja_lab_1_pliki_zrodlowe/opt_alg.cpp
@@ -58,82 +58,6 @@ double* expansion(matrix(*ff)(matrix, matrix, matrix), double x0, double d, doub
     }
 }
 
-//double* expansion(matrix(*ff)(matrix, matrix, matrix), double x0, double d, double alpha, int Nmax, matrix ud1, matrix ud2)
-//{
-//	try
-//	{
-//		double* p = new double[2]{ 0,0 };
-//		//Tu wpisz kod funkcji
-//
-//        double i=0;
-//        double x1 = x0+d;
-//
-//        solution X0(x0),X1(x1*(x0+d));
-//        matrix fx0 = X0.fit_fun(ff,ud1,ud2);
-//        matrix fx1 = X1.fit_fun(ff,ud1,ud2);
-//        fx0 = X0.y();
-//        fx1 = X1.y();
-//
-//        if(fx1 == fx0){
-//            p[0]=x0;
-//            p[1]=x1;
-//            return p;
-//        }
-//        if(fx1 > fx0){
-//            d = -d;
-//            x1 = x0+d;
-//            if(fx1 >= fx0){
-//                p[0] = x1;
-//                p[1] = x0-d;
-//                return p;
-//            }
-//        }
-//
-//        matrix fxp1;
-//        matrix fxi;
-//        double xp1 = x0;
-//        double xm1 = x0;
-//        double xi = x0;
-//        do{
-//            if(i > Nmax){
-//                throw("Error if(i>nMax)!");
-//            }
-//            i = i+1;
-//            xm1 = xi; //mniejsza to poprzednia
-//            xi = xp1; //obecna to poprzednia większa
-//            xp1 = x0 + pow(alpha, i) * d; //zwiększamy +1
-//
-//            solution Xi(xi);
-//            fxi = Xi.fit_fun(ff,ud1,ud2);
-//            solution Xp1(xp1);
-//            fxp1 = Xp1.fit_fun(ff,ud1,ud2);
-//            fxi = Xi.y();
-//            fxp1 = Xp1.y();
-//
-//        }while(fxi >= fxp1);
-//
-//        if(d>0){
-//            p[0] = xm1;
-//            p[1] = xp1;
-//            return p;
-//        }
-//        p[0] = xp1;
-//        p[1] = xm1;
-//		return p;
-//	}
-//	catch (string ex_info)
-//	{
-//		throw ("double* expansion(...):\n" + ex_info);
-//	}
-//}
-double gcd(int a, int b)
-{
-    if (a == 0)
-        return b;
-    return gcd(b % a, a);
-}
-
-
 double phi(double n)
 {
     return (pow((1.0 + sqrt(5.0)) / 2.0, n) / sqrt(5.0) ) - (pow((1.0 - sqrt(5.0)) / 2.0, n) / sqrt(5.0));
@@ -141,54 +65,53 @@ double phi(double n)
 
 solution fib(matrix(*ff)(matrix, matrix, matrix), double a, double b, double epsilon, matrix ud1, matrix ud2)
 {
+    // CORRECT VALUE FOR O1
+    // x = 0.00208886
+    // y = 1.15537e-07
+
 	try
 	{
 		solution Xopt;
-		//Tu wpisz kod funkcji
+        /* double k = 0;
 
-
-        //szukanie k
-        double k = 0;
-      //  φk > (b - a) / ε
-       int x=0;
-
-        while(2<5){
+        // THIS APPROACH IS GIVING WEIRD VALUES
+        int x=0;
+        while(true){
             if(phi(x)>(b-a)/epsilon){
                 k = x*1.0;
                 break;
             }
             x++;
-        }
-//if(phi(3)<=(b-a)/epsilon){
-//    cout<<"True"<<endl;
-//}else{
-//    cout<<"False"<<endl;
-//}
-        //cout<<phi(7)<<endl;
-        double a0 = a;
-        double b0 = b;
-        double c0 = b0 - phi(k-1) / phi(k)*(b0-a0);
-        double d0 = a0+b0-c0;
-        double ci=c0;
-        double di=d0;
-        double ai=a0;
-        double bi=b0;
-        double aip1=a0;
-        double bip1=b0;
-        double cip1=c0;
-        double dip1=d0;
-        for(int i=0; i<k-3;i++){
-            if(f(ci,ff) < f(di,ff)){
-                aip1 = ai;
-                bip1 = di;
-            }else{
-                bip1 = bi;
-                aip1 = ci;
+        } */
+
+        int k = static_cast<int>(ceil(log2(sqrt(5) * (b-a)/epsilon)/log2((1 + sqrt(5))/ 2)));
+
+        int *F = new int[k] {1, 1};
+        for (int i = 2; i < k; ++i)
+            F[i] = F[i-2] + F[i-1];
+
+        solution A(a), B(b), C, D;
+        C.x = B.x - 1.0 * F[k-2] / F[k-1] * (B.x - A.x);
+        D.x = A.x + B.x - C.x;
+
+        C.fit_fun(ff, ud1, ud2);
+        D.fit_fun(ff, ud1, ud2);
+
+        for (int i = 0;i<=k-3;i++) {
+            if (C.y < D.y) {
+                B = D;
+            } else {
+                A = C;
             }
-            cip1 = bip1 - phi(k-i-2) / phi(k-i-1)*(bip1 - aip1);
-            dip1 = aip1 + bip1 - cip1;
+            C.x = B.x - 1.0 * F[k-i-2]/F[k-i-1] * (B.x - A.x);
+            D.x = A.x + B.x - C.x;
+            C.fit_fun(ff, ud1, ud2);
+            D.fit_fun(ff, ud1, ud2);
+
+            ud1.add_row((B.x - A.x)());
         }
-        Xopt = cip1;
+
+        Xopt = C;
 		return Xopt;
 	}
 	catch (string ex_info)
@@ -361,3 +284,30 @@ solution EA(matrix(*ff)(matrix, matrix, matrix), int N, matrix limits, int mi, i
 		throw ("solution EA(...):\n" + ex_info);
 	}
 }
+
+/*
+ * double a0 = a;
+        double b0 = b;
+        double c0 = b0 - phi(k-1) / phi(k)*(b0-a0);
+        double d0 = a0+b0-c0;
+        double ci=c0;
+        double di=d0;
+        double ai=a0;
+        double bi=b0;
+        double aip1=a0;
+        double bip1=b0;
+        double cip1=c0;
+        double dip1=d0;
+        for(int i=0; i<k-3;i++){
+            if(f(ci,ff) < f(di,ff)){
+                aip1 = ai;
+                bip1 = di;
+            }else{
+                bip1 = bi;
+                aip1 = ci;
+            }
+            cip1 = bip1 - phi(k-i-2) / phi(k-i-1)*(bip1 - aip1);
+            dip1 = aip1 + bip1 - cip1;
+        }
+        Xopt = cip1;
+ * */
--- Optymalizacja/optymalizacja_lab_1_pliki_zrodlowe/user_funs.cpp
+++ Optymalizacja/optymalizacja_lab_1_pliki_zrodlowe/user_funs.cpp
@@ -1,4 +1,5 @@
 #include"user_funs.h"
+#include"ode_solver.h"
 #include <math.h>
 
 matrix ff(matrix x, matrix ud1, matrix ud2){
@@ -6,34 +7,26 @@ matrix ff(matrix x, matrix ud1, matrix ud2){
     y = -cos(0.1 * x()) * exp(-pow(0.1 * x() - 2 * M_PI,2)) + 0.002 * pow(0.1 * x(),2);
     return y;
 }
-matrix df(double t, double a, double b, double g, matrix ud1, matrix ud2,matrix Y){
-    matrix dY(3,1);
-    double PA;
-    double PB;
-    double DB;
-    double fin;
-    double Tin;
-    double Ta;
-    double faout = a*b*m2d(ud2)*sqrt(2*g*Y(0)/PA);
-    double fbout = a*b*DB*sqrt(2*g*Y(1)/PB);
 
-    dY(0) = -faout;
-    dY(1) = faout+fin-fbout;
-    dY(2) = fin/Y(1)*(Tin-Y(2))+((faout/Y(1))*(Ta-Y(2)));
-    
+matrix df(double t, matrix y, matrix ud1, matrix ud2){
+    double a = 0.98, b = 0.63, g = 9.81, PA = 1, TA = 90, PB = 1, DB = 0.00365665, Fin = 0.01, Tin = 10;
+    matrix dY(3, 1);
+    double FAout = y(0) > 0 ? a * b * ud2() * sqrt(2 * g * y(0) / PA) : 0;
+    double FBout = y(1) > 0 ? a * b * DB * sqrt(2 * g * y(1) / PB) : 0;
+    dY(0) = -FAout;
+    dY(1) = FAout + Fin - FBout;
+    dY(2) = Fin / y(1) * (Tin - y(2)) + FAout / y(1) * (TA - y(2));
+
     return dY;
 }
+
 matrix fR(matrix x, matrix ud1, matrix ud2){
-    double y;
-    matrix Y0 = matrix(3,new double[3]{5,1,10});
-    matrix *Y = solve_ode(df,0,1,1000,Y0,ud1,x);
+    matrix Y0 = matrix(3, new double[3]{ 5,1,10 });
+    matrix* Y = solve_ode(df, 0, 1, 1000, Y0, ud1, x);
     int n = get_len(Y[0]);
-    double max = Y[1](0,2);
-    for(int i=0;i<n;i++){
-        if(max<Y[1](i,2)){
-            max=Y[1](i,2);
-        }
-    }
-    y=abs(max-50);
-    return y;
+    double max = Y[1](0, 2);
+    for (int i = 1; i < n; ++i)
+        if (max < Y[1](i, 2))
+            max = Y[1](i, 2);
+    return abs(max - 50);
 }
