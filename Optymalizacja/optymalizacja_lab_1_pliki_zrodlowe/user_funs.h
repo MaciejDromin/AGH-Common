c3b99b5a0bb9bfa2dfec57d3dad30ac5b4e70dd9
Maciej Lisowski <macieejl00@gmail.com>
2023-01-13 20:44:45 +0100

    draft of the change

--- Optymalizacja/optymalizacja_lab_1_pliki_zrodlowe/user_funs.h
+++ Optymalizacja/optymalizacja_lab_1_pliki_zrodlowe/user_funs.h
@@ -7,3 +7,7 @@ matrix fR(matrix x, matrix ud1, matrix ud2);
 matrix ff2(matrix x, matrix ud1, matrix ud2);
 matrix df2(double t, matrix y, matrix ud1, matrix ud2);
 matrix fT2(matrix x, matrix ud1, matrix ud2);
+matrix fT3A(matrix x, matrix ud1, matrix ud2);
+matrix fT3B(matrix x, matrix ud1, matrix ud2);
+matrix df3(double t, matrix x, matrix ud1, matrix ud2);
+matrix fr3(matrix x, matrix ud1, matrix ud2);
